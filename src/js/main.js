7b63e4a98197a0493baa53d22c4a04a518fe30a0
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-27 18:38:39 -0400

    Top landing page styling.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
+import 'angular-ui-router';
 import Config from './config';
 
 angular
-  .module('app', ['ui-router'])
+  .module('app', ['ui.router'])
   .config(Config);
