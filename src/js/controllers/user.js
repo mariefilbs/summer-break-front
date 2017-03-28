94d62b14fdfea375280060a8fdf192611b11bf97
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-28 16:54:26 -0400

    Toggled signin/signout.

--- src/js/controllers/user.js
+++ src/js/controllers/user.js
@@ -1,19 +1,12 @@
 import $ from 'jquery';
 
 function UserController ($scope, $http, $cookies, $state, SERVER, $rootScope) {
-  $scope.notifications = [];
   console.log($cookies);
-
-  $scope.removeMsg = (msg) => {
-    var removed = $scope.notifications.filter(x => x != msg);
-    $scope.notifications = removed;
-  }
 
   $scope.register = (user) => {
     console.log(user);
     $http.post(`${SERVER}/users`, user).then(resp => {
-      var message = `Created new user: ${resp.data.username}`;
-      $scope.notifications.push(message);
+      $state.go('root.login');
     }).catch(error => {
        console.log(error);
     });
@@ -24,7 +17,7 @@ function UserController ($scope, $http, $cookies, $state, SERVER, $rootScope) {
     $http.post(`${SERVER}/login`, user).then(resp => {
       $rootScope.loggedIn = true;
       $cookies.put('access-token', resp.data.token);
-      $state.go('/home');
+      $state.go('root.home');
     }).catch(error => {
         console.log(error);
     });
