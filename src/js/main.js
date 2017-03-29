5328a6932dc7d21bdf10f5ae2cc1360fdb4ba7c3
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-29 14:52:49 -0400

    Imported setup to main.js.

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ import angular from 'angular';
 import 'angular-ui-router';
 import 'angular-cookies';
 import Config from './config';
-import SERVER from './server';
+import SERVER from './server'
+import setup from './setup';
 
 import UserController from './controllers/user';
 import LayoutController from './controllers/layout';
@@ -11,6 +12,7 @@ import EventController from './controllers/events';
 angular
   .module('app', ['ui.router', 'ngCookies'])
   .config(Config)
+  .run(setup)
   .constant('SERVER', SERVER)
   .controller('UserController', UserController)
   .controller('LayoutController', LayoutController)
