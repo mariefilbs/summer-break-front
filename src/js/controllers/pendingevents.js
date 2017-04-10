36873d366e10e27acdae171b6fcc6905368a220c
Marie Filbey <patriciamariefilbey@gmail.com>
2017-04-10 12:15:53 -0400

    Event Service folder added and styling/holding tank work.

--- src/js/controllers/pendingevents.js
+++ src/js/controllers/pendingevents.js
@@ -1,7 +1,8 @@
-function PendingEventsController ($scope, $http, $state, $stateParams, SERVER) {
+function PendingEventsController ($scope, $http, $state, $stateParams, SERVER, EventService) {
   $scope.pendingEvents = [];
 
   function init () {
+    // EventService.fetchPending().then(resp => { $scope.pendingEvents = resp.data })
     $http.get(`${SERVER}/activities/all/pending`).then(resp => {
       $scope.pendingEvents = resp.data;
       console.log(resp.data);
@@ -9,41 +10,42 @@ function PendingEventsController ($scope, $http, $state, $stateParams, SERVER) {
   }
   init();
 
-  $scope.addEvent = (data) => {
-    let id = $stateParams.id;
-    $http.put(`${SERVER}/activities/${id}`).then(resp => {
-      console.log(resp.data);
-    $scope.isActivity = true;
-  })
-}
-
-
-  $scope.updateEvent = (id) => {
-    $http.put(`${SERVER}/activities/${id}`).then(resp => {
-      console.log(resp.data);
+  $scope.approve = (id) => {
+    $http.put(`${SERVER}/activities/${id}`, { isActivity: true }).then(resp => {
+      $scope.isActivity = true;
     })
+
   }
 
+
+  // $scope.updateEvent = (id, data) => {
+  //   $http.put(`${SERVER}/activities/${id}`, data).then(resp => {
+  //     console.log(resp.data);
+  //   })
+  // }
+
   $scope.enableEditor = (pending) => {
-    pending.editing = true;
+    pending.isEditing = true;
   };
 
   $scope.disableEditor = function (pending) {
-    pending.editing = false;
+    pending.isEditing = false;
   };
 
-  $scope.save = function() {
-    $scope.disableEditor();
+  $scope.save = function(pending) {
+    pending.isEditing = false;
+    EventService.updateEvent(pending).then(resp => { console.log("Save successful"); })
   };
 
   $scope.deleteEvent = (id) => {
     $http.delete(`${SERVER}/activities/${id}`).then(resp => {
       console.log(resp.data);
+      $state.reload();
     })
   }
 }
 
 
-PendingEventsController.$inject = ['$scope', '$http', '$state', '$stateParams', 'SERVER'];
+PendingEventsController.$inject = ['$scope', '$http', '$state', '$stateParams', 'SERVER', 'EventService'];
 
 export default PendingEventsController;
