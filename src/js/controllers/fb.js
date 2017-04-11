0eece8ea1c1fcba8a31739856d3798436b1e933a
Marie Filbey <patriciamariefilbey@gmail.com>
2017-04-11 16:15:03 -0400

    Stored Facebook info in userInfo object.

--- src/js/controllers/fb.js
+++ src/js/controllers/fb.js
@@ -3,6 +3,16 @@ function FBController ($cookies, $state, $stateParams, $http) {
   let fbtoken = $stateParams.token;
   $cookies.put('access-token', fbtoken);
   $http.defaults.headers.common['access-token'] = fbtoken;
+  console.log(fbtoken);
+
+  $http.get('/me').then(resp => {
+    console.log(resp.data);
+    let myObject = {
+      firstName: resp.data.name,
+      email: resp.data.email
+    }
+    $cookies.putObject('userInfo', myObject);
+  })
 
   $state.go('root.home');
 }
@@ -10,7 +20,3 @@ function FBController ($cookies, $state, $stateParams, $http) {
 FBController.$inject = ['$cookies', '$state', '$stateParams', '$http'];
 
 export default FBController;
-
-
-//take .token and store in cookies
-//state.go to home
